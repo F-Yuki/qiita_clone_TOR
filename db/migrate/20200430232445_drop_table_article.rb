0c009988e9f54c0f1b53c5c288a9b98eae7c7c0e
節原有貴 <fushihara@fushiharayukinoMacBook-Air.local>
2020-05-02 17:58:54 +0900

    circleciのconfig.yml設定13

--- db/migrate/20200430232445_drop_table_article.rb
+++
@@ -1,8 +0,0 @@
-class DropTableArticle < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :articles
-  end
-
-  def down
-  end
-end
--- db/migrate/20200430232800_create_articles.rb
+++ db/migrate/20200430232800_create_articles.rb
@@ -1,13 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
-  def up
+  def change
     create_table :articles do |t|
       t.string :title
       t.text :body
 
       t.timestamps
     end
-  end
-
-  def down
   end
 end
