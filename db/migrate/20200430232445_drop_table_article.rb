e544b59cc53cce8b3dc85b20ea4cc198856bbee0
節原有貴 <fushihara@fushiharayukinoMacBook-Air.local>
2020-05-02 13:35:28 +0900

    circleciのconfig.yml設定6

--- db/migrate/20200430232445_drop_table_article.rb
+++ db/migrate/20200430232445_drop_table_article.rb
@@ -1,11 +1,9 @@
 class DropTableArticle < ActiveRecord::Migration[5.2]
-
   def up
     drop_table :articles
   end
 
   def down
-    fail ActiveRecord::IrreversibleMigration
+    raise ActiveRecord::IrreversibleMigration
   end
-
 end
